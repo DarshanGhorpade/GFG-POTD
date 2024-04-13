252469d0f8456ec9729fa6915e87bc155aa5a7a2
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-04-13 17:46:46 +0530

    Update 12_04_2024_Sum_Of_Products.cpp

--- 12_04_2024_Sum_Of_Products.cpp
+++ 12_04_2024_Sum_Of_Products.cpp
@@ -10,7 +10,33 @@ using namespace std;
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
+class Solution {
+  public:
+    long long pairAndSum(int n, long long arr[]) {
+        // code here
+        long long ans = 0;
+        /*
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                ans += arr[i] & arr[j];
+            }
+        }
+        */
+        for (int i = 0; i < 32; i++) {
+        // Count number of elements with i'th bit set
+        long long k = 0; // Initialize the count
+        for (int j = 0; j < n; j++)
+            if ((arr[j] & (1 << i)))
+                k++;
+ 
+        // There are k set bits, means k(k-1)/2 pairs.
+        // Every pair adds 2^i to the answer. Therefore,
+        // we add "2^i * [k*(k-1)/2]" to the answer.
+        ans += (1 << i) * (k * (k - 1) / 2);
+        }
+        return ans;
+    }
+};
 
 //{ Driver Code Starts.
 int main() {
