222b5ccf0b8461222cdae4896a86833b39111a0c
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-06-21 23:22:21 +0530

    Create 21_06_2024_CompareTwoFractions.cpp

---
+++ 2024/June/21_06_2024_CompareTwoFractions.cpp
@@ -0,0 +1,74 @@
+/*
+You are given a string str containing two fractions a/b and c/d, compare them and return the greater. If they are equal, then return "equal".
+
+Note: The string str contains "a/b, c/d"(fractions are separated by comma(,) & space( )). 
+
+Examples
+
+Input: str = "5/6, 11/45"
+Output: 5/6
+Explanation: 5/6=0.8333 and 11/45=0.2444, So 5/6 is greater fraction.
+Input: str = "8/1, 8/1"
+Output: equal
+Explanation: We can see that both the fractions are same, so we'll return a string "equal".
+Input: str = "10/17, 9/10"
+Output: 9/10
+Explanation: 10/17 = 0.588 & 9/10 = 0.9, so the greater fraction is "9/10".
+Expected Time Complexity: O(len|str|)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+0<=a,c<=103
+1<=b,d<=103
+*/
+
+//{ Driver Code Starts
+
+#include <iostream>
+#include <regex>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string compareFrac(string str) {
+        // Code here
+        int a, b, c, d;
+        stringstream ss(str);
+        char slash, comma;
+        ss >> a >> slash >> b >> comma;
+        ss >> c >> slash >> d;
+        int left = a * d;
+        int right = b * c;
+    
+        if (left > right) {
+            return to_string(a) + "/" + to_string(b);
+        } else if (left < right) {
+            return to_string(c) + "/" + to_string(d);
+        } else {
+            return "equal";
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    Solution ob;
+    int t;
+    cin >> t;
+    cin.ignore();
+    for (int i = 0; i < t; i++) {
+
+        string str;
+        getline(cin, str);
+
+        cout << ob.compareFrac(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
