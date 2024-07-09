bf36058a432b27362c70c0c5a2d00f34ae5733d8
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-07-09 23:54:10 +0530

    Create 09_07_2024_ClosestThreeSum.cpp

---
+++ 2024/July/09_07_2024_ClosestThreeSum.cpp
@@ -0,0 +1,89 @@
+/*
+Problem Link ---> https://www.geeksforgeeks.org/problems/three-sum-closest/1
+Difficulty   ---> Medium
+
+Given an array, arr of integers, and another number target, find three integers in the array such that their sum is closest to the target. Return the sum of the three integers.
+
+Note: If there are multiple solutions, return the maximum one.
+
+Examples :
+
+Input: arr[] = [-7, 9, 8, 3, 1, 1], target = 2
+Output: 2
+Explanation: There is only one triplet present in the array where elements are -7,8,1 whose sum is 2.
+Input: arr[] = [5, 2, 7, 5], target = 13
+Output: 14
+Explanation: There is one triplet with sum 12 and other with sum 14 in the array. Triplet elements are 5, 2, 5 and 2, 7, 5 respectively. Since abs(13-12) ==abs(13-14) maximum triplet sum will be preferred i.e 14.
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+3 ≤ arr.size() ≤ 103
+-105 ≤ arr[i] ≤ 105
+1 ≤ target ≤ 105
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// arr : given vector of elements
+// target : given sum value
+
+class Solution {
+  public:
+    int threeSumClosest(vector<int> arr, int target) {
+        // Your code goes here
+        int dif=INT_MAX,ans=INT_MIN;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            int start=i+1;
+            int end=arr.size()-1;
+            while(start<end){
+                int sum=arr[i]+arr[start]+arr[end];
+                if(sum==target)return sum;
+                else if(sum>target) end--;
+                else start++;
+                if(abs(sum-target)<dif){
+                    dif=abs(sum-target);
+                    ans=sum;
+                }
+                else if(abs(sum-target)==dif){
+                    ans=max(ans,sum);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        // cin.ignore();
+        // if (f)
+        //     return 0;
+        Solution ob;
+        cout << ob.threeSumClosest(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
