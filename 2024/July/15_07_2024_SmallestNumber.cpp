6b435e443608310bad3c692c3544254f0b8a0f9d
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-07-15 18:17:13 +0530

    Create 15_07_2024_SmallestNumber.cpp

---
+++ 2024/July/15_07_2024_SmallestNumber.cpp
@@ -0,0 +1,70 @@
+/*
+Problem Link ---> https://www.geeksforgeeks.org/problems/smallest-number5829/1
+Difficulty   ---> 
+
+Given two integers s and d. The task is to find the smallest number such that the sum of its digits is s and the number of digits in the number are d. Return a string that is the smallest possible number. If it is not possible then return -1.
+
+Examples:
+
+Input: s = 9, d = 2
+Output: 18 
+Explanation: 18 is the smallest number possible with the sum of digits = 9 and total digits = 2.
+Input: s = 20, d = 3 
+Output: 299 
+Explanation: 299 is the smallest number possible with the sum of digits = 20 and total digits = 3.
+Expected Time Complexity: O(d)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ s ≤ 100
+1 ≤ d ≤ 6
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string smallestNumber(int s, int d) {
+        // code here
+        if (9 * d < s) {
+            return "-1";
+        }
+        if (s > 9) {
+            return smallestNumber(s - 9, d - 1) + "9";
+        }
+        if (d == 1){
+            return to_string(s);
+        }
+        if (s > 1) {
+            return smallestNumber(1, d - 1) + to_string(s - 1);
+        }
+        
+        return smallestNumber(1, d - 1) + "0";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int s, d;
+        cin >> s >> d;
+        Solution ob;
+        cout << ob.smallestNumber(s, d) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
