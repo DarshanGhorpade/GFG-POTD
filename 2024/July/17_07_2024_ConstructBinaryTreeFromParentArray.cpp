17d2392413ba3e25d35cd3a09e0bf14796938c12
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-07-17 22:41:09 +0530

    Create 17_07_2024_ConstructBinaryTreeFromParentArray.cpp

---
+++ 2024/July/17_07_2024_ConstructBinaryTreeFromParentArray.cpp
@@ -0,0 +1,168 @@
+/*
+Problem Link ---> https://www.geeksforgeeks.org/problems/construct-binary-tree-from-parent-array/1
+Difficulty   ---> Medium
+
+Given an array parent that is used to represent a tree. The array indices (0-based indexing) are the values of the tree nodes and parent[i] denotes the parent node of a particular node. The parent of the root node would always be -1, as there is no parent for the root. Construct the standard linked representation of Binary Tree from this array representation and return the root node of the constructed tree.
+
+Note: If two elements have the same parent, the one that appears first in the array will be the left child and the other is the right child. You don't need to print anything, the driver code will print the level order traversal of the returned root node to verify the output.
+
+Examples:
+
+Input: parent[] = [-1, 0, 0, 1, 1, 3,5]
+Output: 0 1 2 3 4 5 6
+Explanation: the tree generated
+will have a structure like 
+          0
+        /   \
+       1     2
+      / \
+     3   4
+    /
+   5
+ /
+6
+Input: parent[] = [2, 0, -1]
+Output: 2 0 1
+Explanation: the tree generated will
+have a sturcture like
+             2
+            /   
+           0      
+          /   
+         1     
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 ≤ parent.size() ≤ 103
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void sort_and_print(vector<int>& v) {
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    v.clear();
+}
+
+void printLevelOrder(struct Node* root) {
+    vector<int> v;
+    queue<Node*> q;
+
+    q.push(root);
+
+    Node* next_row = NULL;
+
+    while (!q.empty()) {
+        Node* n = q.front();
+        q.pop();
+
+        if (n == next_row) {
+            sort_and_print(v);
+            next_row = NULL;
+        }
+
+        v.push_back(n->data);
+
+        if (n->left) {
+            q.push(n->left);
+            if (next_row == NULL)
+                next_row = n->left;
+        }
+
+        if (n->right) {
+            q.push(n->right);
+            if (next_row == NULL)
+                next_row = n->right;
+        }
+    }
+    sort_and_print(v);
+}
+
+Node* createTree(int parent[], int n);
+
+/* Driver program to test above function*/
+
+
+// } Driver Code Ends
+/* node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to construct binary tree from parent array.
+    Node *createTree(vector<int> parent) {
+        unordered_map<int,Node*> nodes;
+        for(int i =0;i< parent.size();i++){
+            nodes[i] =new Node(i);
+        }
+        int rootval =-1;
+        for(int i =0;i< parent.size();i++){
+            int Par =parent[i];
+            int Child =i;
+            if(Par == -1){
+                rootval =i;
+                continue;
+            }
+            if(nodes[Par]->left == nullptr){
+                nodes[Par]->left =nodes[Child];
+            }else{
+                nodes[Par]->right =nodes[Child];
+            }
+        }
+        return nodes[rootval];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+
+        Solution ob;
+
+        Node* res = ob.createTree(a);
+
+        printLevelOrder(res);
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
