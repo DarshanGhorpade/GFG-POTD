f08b2c4189b0d79d18110aceb42dee9bbec67f2d
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-07-25 17:56:43 +0530

    Create 25_07_2024_ArrayToBST.cpp

---
+++ 2024/July/25_07_2024_ArrayToBST.cpp
@@ -0,0 +1,171 @@
+/*
+Problem Link ---> https://www.geeksforgeeks.org/problems/array-to-bst4443/1
+Difficulty   ---> Easy
+
+Given a sorted array. Convert it into a Height Balanced Binary Search Tree (BST). Return the root of the BST.
+
+Height-balanced BST means a binary tree in which the depth of the left subtree and the right subtree of every node never differ by more than 1.
+
+Note: The driver code will check the BST, if it is a Height-balanced BST, the output will be true otherwise the output will be false.
+
+Examples :
+
+Input: nums = [1, 2, 3, 4]
+Output: true
+Explanation: The preorder traversal of the following BST formed is [2, 1, 3, 4]:
+           2
+         /   \
+        1     3
+               \
+                4
+Input: nums = [1, 2, 3, 4, 5, 6, 7]
+Ouput: true
+Explanation: The preorder traversal of the following BST formed is [4, 2, 1, 3, 6, 5, 7]:
+        4
+       / \
+      2   6
+     / \   / \
+    1 3  5 7
+Expected Time Complexity: O(n)
+Expected Auxillary Space: O(n)
+
+Constraints:
+1 ≤ nums.size() ≤ 105
+1 ≤ nums[i] ≤ 105
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+        : data(val)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+
+bool help(Node* root, long long int up, long long int l) {
+    if (root->data >= up || root->data <= l)
+        return false;
+    bool ans = true;
+    if (root->left)
+        ans = help(root->left, root->data, l);
+    if (ans && root->right)
+        ans = help(root->right, up, root->data);
+    return ans;
+}
+
+bool isValidBST(Node* root) {
+    return help(root, 3147483648, -3147483649);
+}
+
+int Height(Node* root) {
+
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = Height(root->left);
+
+    int rightHight = Height(root->right);
+
+    if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)
+        return -1;
+
+    return max(leftHeight, rightHight) + 1;
+}
+
+bool isBalanced(Node* root) {
+
+    if (root == NULL)
+        return true;
+
+    if (Height(root) == -1)
+        return false;
+    return true;
+}
+
+void inorder(Node* root, vector<int>& v) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+        : data(val)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+*/
+
+class Solution {
+  public:
+    Node* solution(vector<int>& nums,int left,int right){
+        if(left>right) return NULL;
+        if(left==right){
+            return new Node(nums[left]);
+        }
+        int mid=(left+right)/2;
+        Node* root=new Node(nums[mid]);
+        root->left=solution(nums,left,mid-1);
+        root->right=solution(nums,mid+1,right);
+        return root;
+    }
+    
+    Node* sortedArrayToBST(vector<int>& nums) {
+        // Code here
+        return solution(nums,0,nums.size()-1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        Node* a = ob.sortedArrayToBST(arr);
+        vector<int> v;
+        inorder(a, v);
+
+        if (!isValidBST(a) or v != arr) {
+            cout << "false" << endl;
+            return 0;
+        }
+        bool f = isBalanced(a);
+
+        if (f)
+            cout << "true";
+        else
+            cout << "false";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
