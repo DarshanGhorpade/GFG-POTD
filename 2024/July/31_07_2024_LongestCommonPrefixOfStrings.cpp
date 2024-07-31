e76edad15ff37ea40f7603c7d38cce538384e274
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2024-07-31 17:50:39 +0530

    Create 31_07_2024_LongestCommonPrefixOfStrings.cpp

---
+++ 2024/July/31_07_2024_LongestCommonPrefixOfStrings.cpp
@@ -0,0 +1,72 @@
+/*
+Problem Link ---> https://www.geeksforgeeks.org/problems/longest-common-prefix-in-an-array5129/1
+Difficulty   ---> Easy
+
+Given an array of strings arr. Return the longest common prefix among all strings present in the array. If there's no prefix common in all the strings, return "-1".
+
+Examples :
+
+Input: arr[] = ["geeksforgeeks", "geeks", "geek", "geezer"]
+Output: gee
+Explanation: "gee" is the longest common prefix in all the given strings.
+Input: arr[] = ["hello", "world"]
+Output: -1
+Explanation: There's no common prefix in the given strings.
+Expected Time Complexity: O(n*min(|arri|))
+Expected Space Complexity: O(min(|arri|))
+
+Constraints:
+1 ≤ |arr| ≤ 103
+1 ≤ |arr[i]| ≤ 103
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    string longestCommonPrefix(vector<string> arr) {
+        // your code here
+        if (arr.empty()) return "-1";
+        
+        string prefix = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            while (arr[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+                if (prefix.empty()) return "-1";
+            }
+        }
+        
+        return prefix.empty() ? "-1" : prefix;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<string> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        string number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.longestCommonPrefix(arr) << endl;
+    }
+}
+
+// } Driver Code Ends
