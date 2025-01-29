563613b5ffdf8281dfa13a2e13c8164f109c8ed9
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2025-01-29 17:16:43 +0530

    Added codefile 29012025_ImplementPower.cpp

---
+++ 2025/January/29012025_ImplementPower.cpp
@@ -0,0 +1,45 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/powx-n/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    double power(double b, int e) {
+        // code here
+        double result = 1.0;
+        long long exp = abs((long long)e);
+        while (exp > 0) {
+            if (exp % 2 == 1) result *= b;
+            b *= b;
+            exp /= 2;
+        }
+        return e < 0 ? 1.0 / result : result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    cout << fixed << setprecision(5);
+    int t;
+    cin >> t;
+    while (t--) {
+        double b;
+        cin >> b;
+        int e;
+        cin >> e;
+        Solution ob;
+        cout << ob.power(b, e) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
