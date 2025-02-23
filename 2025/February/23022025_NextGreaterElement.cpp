c2a2cdc07e6ad7fcbdcb71aa69720de95c4b9012
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2025-02-23 19:59:32 +0530

    Added codefile 23022025_NextGreaterElement.cpp

---
+++ 2025/February/23022025_NextGreaterElement.cpp
@@ -0,0 +1,59 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the next greater element for each element of the array.
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        list<int> indices;
+        vector<int> res (arr.size(), -1);
+        for(int i = arr.size()-1; i >= 0; i--){
+            while(!indices.empty() && arr[indices.back()] <= arr[i])    indices.pop_back();
+            if(!indices.empty())    res[i] = arr[indices.back()];
+            indices.push_back(i);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading t
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Reading the entire input line for the array
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.nextLargerElement(a);
+
+        // Print the result in the required format
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
