bb5a589c933c1ca563b56b59e877e88e27983d1b
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-03-04 16:07:09 +0530

    Added codefile 04032025_LongestIncreasingSubsequence.cpp

---
+++ 2025/March/04032025_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,53 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
+        // code here
+        vector<int> ans;
+        for(int num : arr){
+            auto it = lower_bound(ans.begin(), ans.end(), num);
+            if(it == ans.end()) ans.push_back(num);
+            else *it = num;
+        }
+        return ans.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
