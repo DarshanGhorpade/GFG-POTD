00b6daf9da753f5e0e1f6aa4b53a6d4cc5a3d726
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-03-12 17:21:39 +0530

    Added codefile 12032025_MinCostClimbingStairs.cpp

---
+++ 2025/March/12032025_MinCostClimbingStairs.cpp
@@ -0,0 +1,54 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/min-cost-climbing-stairs/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        // Write your code here
+        int n = cost.size();
+        vector<int> f(n + 1);
+        for(int i = 2; i <= n; i++){
+            f[i] = min(f[i - 2] + cost[i - 2], f[i - 1] + cost[i - 1]);
+        }
+        return f[n];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t-- > 0) {
+        string str;
+        getline(cin, str);
+
+        stringstream ss(str);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution sln;
+        int res = sln.minCostClimbingStairs(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
