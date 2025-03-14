43b10137379a82b7f41af51394d8f48100393ac6
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-03-14 09:57:14 +0530

    Added codefile 14032025_CoinChange.cpp

---
+++ 2025/March/14032025_CoinChange.cpp
@@ -0,0 +1,55 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/coin-change2448/1
+// Difficulty   ---> Medium 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int count(vector<int>& coins, int sum) {
+        // code here.
+        int n = sum;
+        unsigned f[n + 1];
+        memset(f, 0, sizeof(f));
+        f[0] = 1;
+        for (int x : coins) {
+            for (int j = x; j <= n; ++j) {
+                f[j] += f[j - x];
+            }
+        }
+        return f[n];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
