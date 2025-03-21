b000f2f3cdb056ad8bc776605e3cd18a57b9b36b
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-03-21 23:58:28 +0530

    Added codefile 21032025_SticklerThief.cpp

---
+++ 2025/March/21032025_SticklerThief.cpp
@@ -0,0 +1,50 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findMaxSum(vector<int>& arr) {
+        // code here
+        int prev2 = 0, prev1 = 0;
+        for (int num : arr) {
+            int temp = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        return prev1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findMaxSum(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
