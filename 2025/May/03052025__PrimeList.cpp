40936c4ab71022a545cd35f3009b2ddf0115b292
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-05-03 12:04:56 +0530

    Added codefile 03052025__PrimeList.cpp

---
+++ 2025/May/03052025__PrimeList.cpp
@@ -0,0 +1,115 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/prime-list--170646/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int val;
+    Node* next;
+
+    Node(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+/*
+class Node{
+public:
+    int val;
+    Node *next;
+    Node(int num){
+        val=num;
+        next=NULL;
+    }
+};
+*/
+
+class Solution {
+    bool isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i <= sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    int nearestPrime(int n) {
+        if (n <= 1) return 2;
+        int left = n, right = n;
+        while (true) {
+            if (isPrime(left)) return left;
+            if (isPrime(right)) return right;
+            left--;
+            right++;
+        }
+    }
+  public:
+    Node *primeList(Node *head) {
+        // code here
+        Node* current = head;
+        while (current != nullptr) {
+            current->val = nearestPrime(current->val);
+            current = current->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        head = ob.primeList(head);
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
