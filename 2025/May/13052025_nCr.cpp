fceed7da9e52a18921d7f07b7607ce9724eb324a
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-05-13 22:48:51 +0530

    Added codefile 13052025_nCr.cpp

---
+++ 2025/May/13052025_nCr.cpp
@@ -0,0 +1,53 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/ncr1019/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+    static const int MOD = 1e9+7;
+    long long modexp(long long x, long long y) {
+        long long res = 1;
+        while (y) {
+            if (y & 1) res = res * x % MOD;
+            x = x * x % MOD;
+            y >>= 1;
+        }
+        return res;
+    }
+  public:
+    int nCr(int n, int r) {
+        // code here
+        if (r > n) return 0;
+        vector<long long> fact(n+1, 1), inv(n+1);
+        for (int i = 1; i <= n; ++i)
+            fact[i] = fact[i-1] * i % MOD;
+        inv[n] = modexp(fact[n], MOD-2);
+        for (int i = n; i > 0; --i)
+            inv[i-1] = inv[i] * i % MOD;
+        return fact[n] * inv[r] % MOD * inv[n-r] % MOD;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
