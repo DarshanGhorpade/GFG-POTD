6c77bbf54daa80f972f2f5650dae7ca82ee95dec
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-05-14 22:25:12 +0530

    Added codefile 14052025_LookAndSayPattern.cpp

---
+++ 2025/May/14052025_LookAndSayPattern.cpp
@@ -0,0 +1,47 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/decode-the-pattern1138/1
+// Difficulty   ---> Medium
+
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string countAndSay(int n) {
+        // code here
+        string res = "1";
+        for (int i = 1; i < n; ++i) {
+            string temp;
+            for (int j = 0; j < res.size(); ) {
+                int k = j;
+                while (k < res.size() && res[k] == res[j]) ++k;
+                temp += to_string(k - j) + res[j];
+                j = k;
+            }
+            res = temp;
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.countAndSay(n) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
