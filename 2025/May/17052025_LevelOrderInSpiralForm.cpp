9cb226e0bf331269fc37d1c2b3d8e40e5b7c8413
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-05-18 15:20:51 +0530

    Added codefile 17052025_LevelOrderInSpiralForm.cpp

---
+++ 2025/May/17052025_LevelOrderInSpiralForm.cpp
@@ -0,0 +1,160 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/level-order-traversal-in-spiral-form/1
+// Difficulty   ---> Easy
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    return new Node(val);
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input string after splitting by space
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+class Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+class Solution {
+  public:
+    vector<int> findSpiral(Node* root) {
+        // code here
+        vector<int> result;
+        if (!root) return result;
+    
+        queue<Node*> q;
+        q.push(root);
+        bool leftToRight = false;
+    
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> level(levelSize);
+    
+            for (int i = 0; i < levelSize; ++i) {
+                Node* node = q.front();
+                q.pop();
+    
+                // Place node value according to current direction
+                int index = leftToRight ? i : levelSize - i - 1;
+                level[index] = node->data;
+    
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+    
+            // Add current level to result
+            result.insert(result.end(), level.begin(), level.end());
+            leftToRight = !leftToRight; // toggle direction
+        }
+    
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    Solution sol; // Create Solution object
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        // Call the findSpiral function from Solution class
+        vector<int> vec = sol.findSpiral(root);
+        for (int x : vec)
+            cout << x << " ";
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
