312538e920a900b4623e3b2799aac1b6c98a5245
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-05-22 23:51:40 +0530

    Added codefile 22052025_MinimumDeletions.cpp

---
+++ 2025/May/22052025_MinimumDeletions.cpp
@@ -0,0 +1,18 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/minimum-deletitions1648/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    int minDeletions(string s) {
+        // code here
+        int n = s.size();
+        vector<int> dp(n), prev(n);
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = 1;
+            for (int j = i + 1; j < n; ++j)
+                dp[j] = s[i] == s[j] ? prev[j - 1] + 2 : max(prev[j], dp[j - 1]);
+            prev = dp;
+        }
+        return n - dp[n - 1];
+    }
+};
