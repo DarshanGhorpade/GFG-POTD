f42fb04cb081fd1612066fa9f9fa12425a73217f
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-06 23:47:10 +0530

    Added codefile 06062025_SearchPattern(Rabin-KarpAlgorithm).cpp

---
+++ 2025/June/06062025_SearchPattern(Rabin-KarpAlgorithm).cpp
@@ -0,0 +1,30 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/search-pattern-rabin-karp-algorithm--141631/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    vector<int> search(string &pat, string &txt) {
+        // code here.
+        int d = 256, q = 101, m = pat.length(), n = txt.length();
+        int ph = 0, th = 0, h = 1;
+        vector<int> res;
+        for (int i = 0; i < m - 1; i++) h = (h * d) % q;
+        for (int i = 0; i < m; i++) {
+            ph = (d * ph + pat[i]) % q;
+            th = (d * th + txt[i]) % q;
+        }
+        for (int i = 0; i <= n - m; i++) {
+            if (ph == th) {
+                bool match = true;
+                for (int j = 0; j < m; j++)
+                    if (txt[i + j] != pat[j]) { match = false; break; }
+                if (match) res.push_back(i + 1);
+            }
+            if (i < n - m) {
+                th = (d * (th - txt[i] * h) + txt[i + m]) % q;
+                if (th < 0) th += q;
+            }
+        }
+        return res;
+    }
+};
