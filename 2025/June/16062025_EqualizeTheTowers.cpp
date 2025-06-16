cab4c85437e3f8c67b83e728c2ce8132eb3a2fa6
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-16 22:37:26 +0530

    Added codefile 16062025_EqualizeTheTowers.cpp

---
+++ 2025/June/16062025_EqualizeTheTowers.cpp
@@ -0,0 +1,23 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/equalize-the-towers2804/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    int minCost(vector<int>& heights, vector<int>& cost) {
+        // code here
+        int l = 0, h = *max_element(heights.begin(), heights.end());
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            long long c1 = 0, c2 = 0;
+            for (int i = 0; i < heights.size(); ++i) {
+                c1 += (long long)abs(heights[i] - m) * cost[i];
+                c2 += (long long)abs(heights[i] - m - 1) * cost[i];
+            }
+            c1 <= c2 ? h = m : l = m + 1;
+        }
+        long long ans = 0;
+        for (int i = 0; i < heights.size(); ++i)
+            ans += (long long)abs(heights[i] - l) * cost[i];
+        return ans;
+    }
+};
