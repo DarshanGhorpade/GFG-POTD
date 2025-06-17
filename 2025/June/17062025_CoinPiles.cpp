0b9e04d24ca0f29b39f71ef3612f0388bc96f7e5
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-17 22:34:30 +0530

    Added codefile 17062025_CoinPiles.cpp

---
+++ 2025/June/17062025_CoinPiles.cpp
@@ -0,0 +1,19 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/coin-piles5152/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    int minimumCoins(vector<int>& a, int k) {
+        // code here
+        sort(a.begin(), a.end());
+        int n = a.size(), t = accumulate(a.begin(), a.end(), 0), res = t, w = 0, p = 0, e = 0;
+        for (int s = 0; s < n; s++) {
+            while (e < n && a[e] - a[s] <= k) w += a[e++];
+            int r = max(0, (t - p - w) - (n - e) * (a[s] + k));
+            res = min(res, p + r);
+            if (e == s) e++; else w -= a[s];
+            p += a[s];
+        }
+        return res;
+    }
+};
