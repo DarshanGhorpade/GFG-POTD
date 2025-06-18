23de970ff9495046b5a754108ccccd8f9b085302
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-18 23:19:10 +0530

    Added codefile 18062025_AllPalindromicPartitions.cpp

---
+++ 2025/June/18062025_AllPalindromicPartitions.cpp
@@ -0,0 +1,31 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/find-all-possible-palindromic-partitions-of-a-string/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    vector<vector<string>> palinParts(string &s) {
+        // code here
+        int n = s.size();
+        vector<vector<bool>> d(n, vector<bool>(n));
+        for (int i = 0; i < n; i++) d[i][i] = true;
+        for (int i = 0; i < n - 1; i++) d[i][i + 1] = s[i] == s[i + 1];
+        for (int l = 3; l <= n; l++)
+            for (int i = 0; i <= n - l; i++) {
+                int j = i + l - 1;
+                d[i][j] = s[i] == s[j] && d[i + 1][j - 1];
+            }
+        vector<vector<string>> r;
+        vector<string> c;
+        function<void(int)> b = [&](int i) {
+            if (i == n) { r.push_back(c); return; }
+            for (int j = i; j < n; j++)
+                if (d[i][j]) {
+                    c.push_back(s.substr(i, j - i + 1));
+                    b(j + 1);
+                    c.pop_back();
+                }
+        };
+        b(0);
+        return r;
+    }
+};
