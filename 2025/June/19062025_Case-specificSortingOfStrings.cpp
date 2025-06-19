adcf40711c8721fec6e55c6b047ef076a36b88c2
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-19 21:59:30 +0530

    Added codefile 19062025_Case-specificSortingOfStrings.cpp

---
+++ 2025/June/19062025_Case-specificSortingOfStrings.cpp
@@ -0,0 +1,24 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/case-specific-sorting-of-strings4845/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    string caseSort(string& s) {
+        // code here
+        int l[26] = {0}, u[26] = {0};
+        for (char c : s) (c & 32) ? l[c - 97]++ : u[c - 65]++;
+        int i = 0, j = 0;
+        for (char &c : s) {
+            if (c & 32) {
+                while (!l[i]) i++;
+                c = i + 97;
+                l[i]--;
+            } else {
+                while (!u[j]) j++;
+                c = j + 65;
+                u[j]--;
+            }
+        }
+        return s;
+    }
+};
