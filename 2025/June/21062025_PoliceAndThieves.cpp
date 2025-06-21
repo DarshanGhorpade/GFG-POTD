e41eaff8c098a1e540e609fe21aa5c68277c04b2
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-21 14:02:27 +0530

    Added codefile 21062025_PoliceAndThieves.cpp

---
+++ 2025/June/21062025_PoliceAndThieves.cpp
@@ -0,0 +1,18 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/police-and-thieves--141631/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    int catchThieves(vector<char> &arr, int k) {
+        // Code here
+        int n = arr.size(), i = 0, j = 0, c = 0;
+        while (i < n && j < n) {
+            while (i < n && arr[i] != 'P') i++;
+            while (j < n && arr[j] != 'T') j++;
+            if (i < n && j < n && abs(i - j) <= k) i++, j++, c++;
+            else if (j < i) j++;
+            else i++;
+        }
+        return c;
+    }
+};
