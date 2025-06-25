8cd7884b8491a0261896dd3a095ef0fcc324e6b0
Darshan <87860114+DarshanGhorpade@users.noreply.github.com>
2025-06-25 23:53:22 +0530

    Added codefile 26062025_CheckIfFrequenciesCanBeEqual.cpp

---
+++ 2025/June/26062025_CheckIfFrequenciesCanBeEqual.cpp
@@ -0,0 +1,22 @@
+// Problem Link ---> https://www.geeksforgeeks.org/problems/check-frequencies4211/1
+// Difficulty   ---> Medium
+
+class Solution {
+  public:
+    bool sameFreq(string& s) {
+        // code here
+        vector<int> freq(26);
+        for (char c : s) freq[c - 'a']++;
+        unordered_map<int, int> m;
+        for (int f : freq) if (f) m[f]++;
+        if (m.size() == 1) return true;
+        if (m.size() == 2) {
+            auto a = m.begin(), b = next(a);
+            int f1 = a->first, c1 = a->second;
+            int f2 = b->first, c2 = b->second;
+            return (f1 == 1 && c1 == 1) || (f2 == 1 && c2 == 1) ||
+                  (abs(f1 - f2) == 1 && ((f1 > f2 && c1 == 1) || (f2 > f1 && c2 == 1)));
+        }
+        return false;
+    }
+};
